8b38bae139333c3ab3906d2bc886be728aa287a1
Fares <ashraffares090@gmail.com>
2021-06-22 19:02:31 +0200

    Add fetch function

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,20 @@
 import './css/styles.css';
+
+
+const callapi = input => {
+    fetch(
+      'http://api.openweathermap.org/data/2.5/weather?q='.concat(input).concat('&APPID=4d7121b3fcc151b2058bedb6f9a71d02'),
+      {
+        mode: 'cors',
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const { name } = data;
+        const { temp } = data.main;
+        const { description } = data.weather[0];
+  
+        addinfo(name, temp, description);
+      })
+      .catch((err) => console.log('error: '.join(err)));
+  };
